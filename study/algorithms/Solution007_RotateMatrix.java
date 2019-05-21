96b1d7aa4a68f7fc90ab443af68bc16f95f9d2e3
abantej <joeward.l.abante@gmail.com>
2019-05-21 21:23:22 +0800

    personal solution for rotate matrix and added test case

--- study/algorithms/Solution007_RotateMatrix.java
+++ study/algorithms/Solution007_RotateMatrix.java
@@ -1,6 +1,7 @@
 package algorithms;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -10,12 +11,16 @@ import org.junit.jupiter.api.Test;
 public class Solution007_RotateMatrix {
 
     public void rotate(int[][] mtx) {
-        for (int i = 0, k = 3; i < 3; i++, k--) {
-            int tmp = mtx[0][i];
-            mtx[0][i] = mtx[k][0];
-            mtx[k][0] = mtx[3][k];
-            mtx[3][k] = mtx[i][3];
-            mtx[i][3] = tmp;
+        int rotation = mtx.length - 1;
+        for (int g = 0, h = rotation; g < rotation; g++, h--) {
+            for (int i = g, k = h; i < rotation; i++, k--) {
+                int tmp = mtx[g][i];
+                mtx[g][i] = mtx[k][g];
+                mtx[k][g] = mtx[h][k];
+                mtx[h][k] = mtx[i][h];
+                mtx[i][h] = tmp;
+            }
+            rotation--;
         }
     }
 
@@ -27,8 +32,10 @@ public class Solution007_RotateMatrix {
         mtx[2] = new int[] {11, 16, 15, 6};
         mtx[3] = new int[] {10,  9,  8, 7};
 
-        for (int[] n: mtx) {
-            for (int m: n) {
+        System.out.println("-----BEFORE-----");
+
+        for (int[] n : mtx) {
+            for (int m : n) {
                 System.out.print(m + "\t");
             }
             System.out.println();
@@ -36,10 +43,63 @@ public class Solution007_RotateMatrix {
 
         rotate(mtx);
 
-        System.out.println("-------------");
+        // expected output
+        int[][] rtd_mtx = new int[4][];
+        rtd_mtx[0] = new int[] {10, 11, 12, 1};
+        rtd_mtx[1] = new int[] { 9, 16, 13, 2};
+        rtd_mtx[2] = new int[] { 8, 15, 14, 3};
+        rtd_mtx[3] = new int[] { 7,  6,  5, 4};
 
-        for (int[] n: mtx) {
-            for (int m: n) {
+        System.out.println("-----AFTER-----");
+
+        for (int[] n : mtx) {
+            for (int m : n) {
+                System.out.print(m + "\t");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < rtd_mtx.length; i++) {
+            for (int k = 0; k < rtd_mtx.length; k++) {
+                assertEquals(mtx[i][k], rtd_mtx[i][k]);
+            }
+        }
+
+        int[][] mtx_5 = new int[5][];
+        mtx_5[0] = new int[]{ 1,  2,  3,  4, 5};
+        mtx_5[1] = new int[]{16, 17, 18, 19, 6};
+        mtx_5[2] = new int[]{15, 24, 25, 20, 7};
+        mtx_5[3] = new int[]{14, 23, 22, 21, 8};
+        mtx_5[4] = new int[]{13, 12, 11, 10, 9};
+
+        System.out.println("-----BEFORE-----");
+
+        for (int[] n : mtx) {
+            for (int m : n) {
+                System.out.print(m + "\t");
+            }
+            System.out.println();
+        }
+
+        rotate(mtx_5);
+
+        int[][] rtd_mtx_5 = new int[5][];
+        rtd_mtx_5[0] = new int[]{13, 14, 15, 16, 1};
+        rtd_mtx_5[1] = new int[]{12, 23, 24, 17, 2};
+        rtd_mtx_5[2] = new int[]{11, 22, 25, 18, 3};
+        rtd_mtx_5[3] = new int[]{10, 21, 20, 19, 4};
+        rtd_mtx_5[4] = new int[]{ 9,  8,  7,  6, 5};
+
+        for (int i = 0; i < rtd_mtx_5.length; i++) {
+            for (int k = 0; k < rtd_mtx_5.length; k++) {
+                assertEquals(mtx_5[i][k], rtd_mtx_5[i][k]);
+            }
+        }
+
+        System.out.println("-----AFTER-----");
+
+        for (int[] n : rtd_mtx_5) {
+            for (int m : n) {
                 System.out.print(m + "\t");
             }
             System.out.println();
