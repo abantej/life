62a313b1c05c6ce19bbb59a02b8cf30db08daeaa
abantej <joeward.l.abante@gmail.com>
2019-05-27 07:36:00 +0800

    starting stack

---
+++ study/algorithms/stack/Stack.java
@@ -0,0 +1,8 @@
+package algorithms.stack;
+
+/**
+ * A stack data structure is precisely what it sounds like: a stack of data. In certain types of problems,
+ * it can be favorable to store data in a stack rather than in an array.
+ */
+public class Stack {
+}
